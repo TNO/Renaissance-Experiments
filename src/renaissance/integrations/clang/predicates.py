d7e5dfa1f8c180a4b1ef0b780b60211e0db1d1bf
Corvino <rosilde.corvino@tno.nl>
2026-09-10 17:35:31 +0200

    Add explicit Clang parser predicates

---
+++ src/renaissance/integrations/clang/predicates.py
@@ -0,0 +1,31 @@
+from renaissance.syntax_tree.node_protocol import NodeProtocol
+from renaissance.syntax_tree.semantic_kind import SemanticKind
+
+TYPE_REFERENCE_KINDS = frozenset({"TypeRef", "TYPE_REF", "type_identifier"})
+DECLARATION_REFERENCE_KINDS = frozenset({"DeclRefExpr", "DECL_REF_EXPR"})
+COMPOUND_STATEMENT_KINDS = frozenset({"CompoundStmt", "COMPOUND_STMT"})
+MACRO_DEFINITION_KINDS = frozenset({"MacroDefinition", "MACRO_DEFINITION"})
+
+
+def is_clang_kind(node: NodeProtocol, *parser_kinds: str) -> bool:
+    return node.parser_kind in parser_kinds
+
+
+def is_clang_type_reference(node: NodeProtocol) -> bool:
+    return node.parser_kind in TYPE_REFERENCE_KINDS
+
+
+def is_clang_declaration_reference(node: NodeProtocol) -> bool:
+    return node.parser_kind in DECLARATION_REFERENCE_KINDS
+
+
+def is_clang_compound_statement(node: NodeProtocol) -> bool:
+    return node.parser_kind in COMPOUND_STATEMENT_KINDS
+
+
+def is_clang_macro_definition(node: NodeProtocol) -> bool:
+    return node.parser_kind in MACRO_DEFINITION_KINDS
+
+
+def has_clang_semantic_kind(node: NodeProtocol, kind: SemanticKind) -> bool:
+    return node.semantic_kind is kind
---
+++ test/c_cpp/test_clang_predicates.py
@@ -0,0 +1,31 @@
+from renaissance.integrations.clang.predicates import (
+    has_clang_semantic_kind,
+    is_clang_compound_statement,
+    is_clang_declaration_reference,
+    is_clang_macro_definition,
+    is_clang_type_reference,
+)
+from renaissance.syntax_tree.semantic_kind import SemanticKind
+
+
+class Node:
+    def __init__(self, parser_kind: str, semantic_kind: SemanticKind = SemanticKind.NODE):
+        self.parser_kind = parser_kind
+        self.semantic_kind = semantic_kind
+
+
+def test_clang_parser_predicates_cover_native_and_json_spellings():
+    assert is_clang_type_reference(Node("TypeRef"))
+    assert is_clang_type_reference(Node("TYPE_REF"))
+    assert is_clang_type_reference(Node("type_identifier"))
+    assert is_clang_declaration_reference(Node("DeclRefExpr"))
+    assert is_clang_declaration_reference(Node("DECL_REF_EXPR"))
+    assert is_clang_compound_statement(Node("CompoundStmt"))
+    assert is_clang_compound_statement(Node("COMPOUND_STMT"))
+
+
+def test_clang_macro_and_semantic_predicates_are_explicit():
+    assert is_clang_macro_definition(Node("MacroDefinition"))
+    assert is_clang_macro_definition(Node("MACRO_DEFINITION"))
+    assert has_clang_semantic_kind(Node("FunctionDecl", SemanticKind.FUNCTION), SemanticKind.FUNCTION)
+    assert not has_clang_semantic_kind(Node("FunctionDecl"), SemanticKind.FUNCTION)
