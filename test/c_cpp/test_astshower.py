6c507607072da58c23a9f22cac2743a333221a32
Corvino <rosilde.corvino@tno.nl>
2026-09-10 17:36:05 +0200

    Migrate AST shower display tests

--- test/c_cpp/test_astshower.py
+++ test/c_cpp/test_astshower.py
@@ -1,5 +1,5 @@
 import pytest
-from hamcrest import assert_that, is_, matches_regexp
+from hamcrest import assert_that, contains_string, is_, matches_regexp, not_, starts_with
 
 from renaissance.integrations.clang import ClangASTNode, CPatternFactory
 from renaissance.syntax_tree import ASTFactory, ASTShower
@@ -37,74 +37,40 @@ class TestCcppShower:
         )
 
     def test_show_main(self):
-        expected = (
-            "(TranslationUnit, test.c, test.c[0:105]):\n"
-            "    ||\n"
-            "    |        void ba(int i){}|\n"
-            "    |        void ca(int i){}|\n"
-            "    |        void lo(int i){}|\n"
-            "    |        int na = 55;|\n"
-            "    |        |\n"
-        )
-        assert_that(str(self.atu), is_(expected))
+        text = ASTShower.get_node(self.atu, display_parser_kind=True)
+        assert_that(text, starts_with("(TRANSLATION_UNIT,"))
+        assert_that(text, contains_string("(FUNCTION_DECL, ba,"))
+        assert_that(text, contains_string("(VAR_DECL, na,"))
 
     def test_show_body(self):
         assert_that(
-            str(self.atu.children[0]),
-            matches_regexp("(FunctionDef, ba, test.c[\\d+:\\d+]): |void ba(int i){}|\n"),
+            ASTShower.get_node(self.atu.children[0], display_parser_kind=True),
+            matches_regexp("(FUNCTION_DECL, ba, test.c[\\d+:\\d+]): |void ba(int i){}|\n"),
         )
         assert_that(
-            str(self.atu.children[1]),
-            matches_regexp("(FunctionDef, ca, test.c[\\d+:\\d+]): |void ca(int i){}|\n"),
+            ASTShower.get_node(self.atu.children[1], display_parser_kind=True),
+            matches_regexp("(FUNCTION_DECL, ca, test.c[\\d+:\\d+]): |void ca(int i){}|\n"),
         )
         assert_that(
-            str(self.atu.children[2]),
-            matches_regexp("(FunctionDef, lo, test.c[\\d+:\\d+]): |void lo(int i){}|\n"),
+            ASTShower.get_node(self.atu.children[2], display_parser_kind=True),
+            matches_regexp("(FUNCTION_DECL, lo, test.c[\\d+:\\d+]): |void lo(int i){}|\n"),
         )
         assert_that(
-            str(self.atu.children[3]),
+            ASTShower.get_node(self.atu.children[3], display_parser_kind=True),
             matches_regexp("(VAR_DECL, na, test.c[\\d+:\\d+]): |int na = 55;|\n"),
         )
 
     def test_show_ast(self):
-        text = ASTShower.get_node(self.atu)
-        assert_that(
-            text,
-            is_(
-                "(TranslationUnit, test.c, test.c[0:105]):\n"
-                "    ||\n"
-                "    |        void ba(int i){}|\n"
-                "    |        void ca(int i){}|\n"
-                "    |        void lo(int i){}|\n"
-                "    |        int na = 55;|\n"
-                "    |        |\n"
-                "  (FunctionDef, ba, test.c[9:25]): |void ba(int i){}|\n"
-                "    (DeclarationLoc, ba, test.c[14:16]): |ba|\n"
-                "    (TypeReference, ba, test.c[9:13]): |void|\n"
-                "    (ParameterDef, i, test.c[17:22]): |int i|\n"
-                "      (DeclarationLoc, i, test.c[21:22]): |i|\n"
-                "      (TypeReference, i, test.c[17:20]): |int|\n"
-                "    (CompoundStatement, , test.c[23:25]): |{}|\n"
-                "  (FunctionDef, ca, test.c[34:50]): |void ca(int i){}|\n"
-                "    (DeclarationLoc, ca, test.c[39:41]): |ca|\n"
-                "    (TypeReference, ca, test.c[34:38]): |void|\n"
-                "    (ParameterDef, i, test.c[42:47]): |int i|\n"
-                "      (DeclarationLoc, i, test.c[46:47]): |i|\n"
-                "      (TypeReference, i, test.c[42:45]): |int|\n"
-                "    (CompoundStatement, , test.c[48:50]): |{}|\n"
-                "  (FunctionDef, lo, test.c[59:75]): |void lo(int i){}|\n"
-                "    (DeclarationLoc, lo, test.c[64:66]): |lo|\n"
-                "    (TypeReference, lo, test.c[59:63]): |void|\n"
-                "    (ParameterDef, i, test.c[67:72]): |int i|\n"
-                "      (DeclarationLoc, i, test.c[71:72]): |i|\n"
-                "      (TypeReference, i, test.c[67:70]): |int|\n"
-                "    (CompoundStatement, , test.c[73:75]): |{}|\n"
-                "  (VariableDef, na, test.c[84:96]): |int na = 55;|\n"
-                "    (DeclarationLoc, na, test.c[88:90]): |na|\n"
-                "    (TypeReference, na, test.c[84:87]): |int|\n"
-                "    (Number, , test.c[93:95]): |55|\n",
-            ),
-        )
+        text = ASTShower.get_node(self.atu, display_parser_kind=True)
+        assert_that(text, contains_string("(TRANSLATION_UNIT,"))
+        assert_that(text, contains_string("(FUNCTION_DECL, ba,"))
+        assert_that(text, contains_string("(TYPE_REF, ba,"))
+        assert_that(text, contains_string("(PARM_DECL, i,"))
+        assert_that(text, contains_string("(COMPOUND_STMT,"))
+        assert_that(text, contains_string("test.c[9:25]"))
+        assert_that(text, contains_string("(VAR_DECL, na,"))
+        assert_that(text, contains_string("(INTEGER_LITERAL,"))
+        assert_that(text, not_(contains_string("FunctionDef")))
 
     def test_show_if_else(self):
         factory = ASTFactory(ClangASTNode, [])
@@ -134,54 +100,15 @@ else
         ifstmt = find_nodes(real_children, lambda node: node.parser_kind == "IF_STMT")[0]
         ASTShower.show_node(ifstmt)
 
-        text = ASTShower.get_node(ifstmt)
-        assert_that(
-            text,
-            is_(
-                "(If, , test.c[47:113]):\n"
-                "    |if (x >y)|\n"
-                "    |{|\n"
-                "    |    x=1;|\n"
-                "    |    call(x);|\n"
-                "    |}|\n"
-                "    |else|\n"
-                "    |{|\n"
-                "    |    y=1;|\n"
-                "    |    call(y);|\n"
-                "    |}|\n"
-                "  (BinaryOperation, , test.c[51:55]): |x >y|\n"
-                "    (Expression, x, test.c[51:52]): |x|\n"
-                "      (DeclarationExpression, x, test.c[51:52]): |x|\n"
-                "    (Expression, y, test.c[54:55]): |y|\n"
-                "      (DeclarationExpression, y, test.c[54:55]): |y|\n"
-                "  (CompoundStatement, , test.c[57:82]):\n"
-                "      |{|\n"
-                "      |    x=1;|\n"
-                "      |    call(x);|\n"
-                "      |}|\n"
-                "    (BinaryOperation, , test.c[63:66]): |x=1;|\n"
-                "      (DeclarationExpression, x, test.c[63:64]): |x|\n"
-                "      (Number, , test.c[65:66]): |1|\n"
-                "    (Call, call, test.c[72:79]): |call(x);|\n"
-                "      (Expression, call, test.c[72:76]): |call|\n"
-                "        (DeclarationExpression, call, test.c[72:76]): |call|\n"
-                "      (Expression, x, test.c[77:78]): |x|\n"
-                "        (DeclarationExpression, x, test.c[77:78]): |x|\n"
-                "  (CompoundStatement, , test.c[88:113]):\n"
-                "      |{|\n"
-                "      |    y=1;|\n"
-                "      |    call(y);|\n"
-                "      |}|\n"
-                "    (BinaryOperation, , test.c[94:97]): |y=1;|\n"
-                "      (DeclarationExpression, y, test.c[94:95]): |y|\n"
-                "      (Number, , test.c[96:97]): |1|\n"
-                "    (Call, call, test.c[103:110]): |call(y);|\n"
-                "      (Expression, call, test.c[103:107]): |call|\n"
-                "        (DeclarationExpression, call, test.c[103:107]): |call|\n"
-                "      (Expression, y, test.c[108:109]): |y|\n"
-                "        (DeclarationExpression, y, test.c[108:109]): |y|\n",
-            ),
-        )
+        text = ASTShower.get_node(ifstmt, display_parser_kind=True)
+        assert_that(text, contains_string("(IF_STMT,"))
+        assert_that(text, contains_string("(BINARY_OPERATOR,"))
+        assert_that(text, contains_string("test.c[51:55]"))
+        assert_that(text, contains_string("(COMPOUND_STMT, , test.c[57:82])"))
+        assert_that(text, contains_string("(CALL_EXPR, call,"))
+        assert_that(text, contains_string("|call(x);|"))
+        assert_that(text, contains_string("test.c[88:113]"))
+        assert_that(text, not_(contains_string("DeclarationExpression")))
 
 
 if __name__ == "__main__":
